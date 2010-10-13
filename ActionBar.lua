bc77393b784253de3af9219e014c24483ea509c4
Hendrik Leppkes <h.leppkes@gmail.com>
2010-10-13 16:55:29 +0200

    Set the buttonlock attribute on the buttons properly.

--- ActionBar.lua
+++ ActionBar.lua
@@ -44,6 +44,8 @@ function ActionBar:UpdateButtonConfig()
 	self:ForAll("SetAttribute", "checkselfcast", Bartender4.db.profile.selfcastmodifier and true or nil)
 	self:ForAll("SetAttribute", "checkfocuscast", Bartender4.db.profile.focuscastmodifier and true or nil)
 	self:ForAll("SetAttribute", "unit2", Bartender4.db.profile.selfcastrightclick and "player" or nil)
+	-- button lock
+	self:ForAll("SetAttribute", "buttonlock", Bartender4.db.profile.buttonlock)
 	-- update state
 	self:ForAll("UpdateState")
 end
