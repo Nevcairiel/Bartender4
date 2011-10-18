65be84d01fb11418257f61ad5f773a6281e1a0e9
Hendrik Leppkes <h.leppkes@gmail.com>
2011-10-18 21:07:24 +0200

    Add Extra Action Bar as introduced in 4.3

---
+++ ExtraActionBar.lua
@@ -0,0 +1,64 @@
+--[[
+	Copyright (c) 2009, Hendrik "Nevcairiel" Leppkes < h.leppkes at gmail dot com >
+	All rights reserved.
+]]
+local L = LibStub("AceLocale-3.0"):GetLocale("Bartender4")
+-- register module
+local ExtraActionBarMod = Bartender4:NewModule("ExtraActionBar", "AceHook-3.0")
+
+-- fetch upvalues
+local Bar = Bartender4.Bar.prototype
+local LBF = LibStub("LibButtonFacade", true)
+
+-- create prototype information
+local ExtraActionBar = setmetatable({}, {__index = Bar})
+
+local table_insert = table.insert
+
+local defaults = { profile = Bartender4:Merge({
+	enabled = true,
+	visibility = {
+		vehicleui = false,
+	},
+}, Bartender4.Bar.defaults) }
+
+function ExtraActionBarMod:OnInitialize()
+	self.db = Bartender4.db:RegisterNamespace("ExtraActionBar", defaults)
+	self:SetEnabledState(self.db.profile.enabled)
+end
+
+function ExtraActionBarMod:OnEnable()
+	if not self.bar then
+		self.bar = setmetatable(Bartender4.ButtonBar:Create("ExtraActionBar", self.db.profile, L["Extra Action Bar"]), {__index = ExtraActionBar})
+		self.bar.content = ExtraActionBarFrame
+		
+		self.bar.content.ignoreFramePositionManager = true
+		self.bar.content:SetParent(self.bar)
+		--self.bar.content:SetFrameLevel(self.bar:GetFrameLevel() + 1)
+	end
+	self.bar:Enable()
+	self:ToggleOptions()
+	self:ApplyConfig()
+end
+
+function ExtraActionBarMod:ApplyConfig()
+	self.bar:ApplyConfig(self.db.profile)
+end
+
+function ExtraActionBar:ApplyConfig(config)
+	Bar.ApplyConfig(self, config)
+
+	if not self.config.position.x then
+		self:ClearSetPoint("BOTTOM", 0, 160)
+		self:SavePosition()
+	end
+
+	self:PerformLayout()
+end
+
+function ExtraActionBar:PerformLayout()
+	self:SetSize(64, 64)
+	local bar = self.content
+	bar:ClearAllPoints()
+	bar:SetPoint("TOPLEFT", 0, 0)
+end
