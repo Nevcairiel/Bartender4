69aeabb8f9bd15b1b8ae6b8f462d0e6abc00cea3
Hendrik Leppkes <h.leppkes@gmail.com>
2011-10-20 19:04:58 +0200

    ExtrActionBar: Use Bar and not ButtonBar

--- ExtraActionBar.lua
+++ ExtraActionBar.lua
@@ -29,7 +29,7 @@ end
 
 function ExtraActionBarMod:OnEnable()
 	if not self.bar then
-		self.bar = setmetatable(Bartender4.ButtonBar:Create("ExtraActionBar", self.db.profile, L["Extra Action Bar"]), {__index = ExtraActionBar})
+		self.bar = setmetatable(Bartender4.Bar:Create("ExtraActionBar", self.db.profile, L["Extra Action Bar"]), {__index = ExtraActionBar})
 		self.bar.content = ExtraActionBarFrame
 		
 		self.bar.content.ignoreFramePositionManager = true
