d28e67b4476ee36c3d862c573ee3ce5568ac2ab4
Hendrik Leppkes <h.leppkes@gmail.com>
2011-12-09 19:03:39 +0100

    Add ptBR locale

---
+++ locale/ptBR.lua
@@ -0,0 +1,8 @@
+-- Bartender4 Locale
+-- Please use the Localization App on WoWAce to Update this
+-- http://www.wowace.com/projects/bartender4/localization/ ;¶
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Bartender4", "ptBR")
+if not L then return end
+
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="comment")@
