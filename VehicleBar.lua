54942794cdb7913626c89579a6df9238d9de08ab
Hendrik Leppkes <h.leppkes@gmail.com>
2012-07-08 11:55:56 +0200

    Re-Add Vehicle Bar with the Leave Vehicle button

--- VehicleBar.lua
+++ VehicleBar.lua
@@ -4,7 +4,7 @@
 ]]
 local L = LibStub("AceLocale-3.0"):GetLocale("Bartender4")
 -- register module
-local VehicleBarMod = Bartender4:NewModule("Vehicle")
+local VehicleBarMod = Bartender4:NewModule("Vehicle", "AceHook-3.0")
 
 -- fetch upvalues
 local ButtonBar = Bartender4.ButtonBar.prototype
@@ -17,8 +17,7 @@ local table_insert = table.insert
 local defaults = { profile = Bartender4:Merge({
 	enabled = true,
 	visibility = {
-		custom = true,
-		customdata = "[target=vehicle,exists]show;hide"
+		vehicleui = false,
 	},
 }, Bartender4.ButtonBar.defaults) }
 
@@ -38,57 +37,33 @@ function VehicleBarMod:OnEnable()
 	end
 	if not self.bar then
 		self.bar = setmetatable(Bartender4.ButtonBar:Create("Vehicle", self.db.profile, L["Vehicle Bar"], true), {__index = VehicleBar})
-		local buttons = {VehicleMenuBarLeaveButton, VehicleMenuBarPitchUpButton, VehicleMenuBarPitchDownButton}
+		local buttons = {MainMenuBarVehicleLeaveButton}
 		self.bar.buttons = buttons
 
-		VehicleBarMod.button_count = 3
+		VehicleBarMod.button_count = #buttons
 
 		for i,v in pairs(buttons) do
 			v:SetParent(self.bar)
-			v:Show()
 			v.ClearSetPoint = self.bar.ClearSetPoint
 		end
 
-		self.bar:SetScript("OnEvent", self.bar.OnEvent)
-		self.bar:RegisterEvent("UNIT_ENTERED_VEHICLE")
-
-		-- setup button skins
-		VehicleMenuBarPitchUpButton:GetNormalTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-Pitch-Up]])
-		VehicleMenuBarPitchUpButton:GetNormalTexture():SetTexCoord(0.21875, 0.765625, 0.234375, 0.78125)
-		VehicleMenuBarPitchUpButton:GetPushedTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-Pitch-Down]])
-		VehicleMenuBarPitchUpButton:GetPushedTexture():SetTexCoord(0.21875, 0.765625, 0.234375, 0.78125)
-
-		VehicleMenuBarPitchDownButton:GetNormalTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-PitchDown-Up]])
-		VehicleMenuBarPitchDownButton:GetNormalTexture():SetTexCoord(0.21875, 0.765625, 0.234375, 0.78125)
-		VehicleMenuBarPitchDownButton:GetPushedTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-PitchDown-Down]])
-		VehicleMenuBarPitchDownButton:GetPushedTexture():SetTexCoord(0.21875, 0.765625, 0.234375, 0.78125)
-
-		VehicleMenuBarLeaveButton:GetNormalTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-Exit-Up]])
-		VehicleMenuBarLeaveButton:GetNormalTexture():SetTexCoord(0.140625, 0.859375, 0.140625, 0.859375)
-		VehicleMenuBarLeaveButton:GetPushedTexture():SetTexture([[Interface\Vehicles\UI-Vehicles-Button-Exit-Down]])
-		VehicleMenuBarLeaveButton:GetPushedTexture():SetTexCoord(0.140625, 0.859375, 0.140625, 0.859375)
+		self:SecureHook("MainMenuBarVehicleLeaveButton_Update")
 	end
 	self.bar:Enable()
 	self:ToggleOptions()
 	self:ApplyConfig()
 end
 
-function VehicleBarMod:OnDisable()
-	Bartender4.modulePrototype.OnDisable(self)
-	VehicleMenuBarPitchUpButton:SetParent(VehicleMenuBar)
-	VehicleMenuBarPitchUpButton:ClearAllPoints()
-	VehicleMenuBarPitchDownButton:SetParent(VehicleMenuBar)
-	VehicleMenuBarPitchDownButton:ClearAllPoints()
-	VehicleMenuBarLeaveButton:SetParent(VehicleMenuBar)
-	VehicleMenuBarLeaveButton:ClearAllPoints()
-end
-
 function VehicleBarMod:ApplyConfig()
 	self.bar:ApplyConfig(self.db.profile)
 end
 
-VehicleBar.button_width = 40
-VehicleBar.button_height = 40
+function VehicleBarMod:MainMenuBarVehicleLeaveButton_Update()
+	self.bar:UpdateButtonLayout()
+end
+
+VehicleBar.button_width = 32
+VehicleBar.button_height = 32
 VehicleBar.LBFOverride = true
 function VehicleBar:ApplyConfig(config)
 	ButtonBar.ApplyConfig(self, config)
@@ -99,28 +74,4 @@ function VehicleBar:ApplyConfig(config)
 	end
 
 	self:UpdateButtonLayout()
-end
-
-function VehicleBar:OnEvent(event, arg1)
-	if event == "UNIT_ENTERED_VEHICLE" then
-		if arg1 == "player" then
-			self:UpdateButtonVisibility()
-		end
-	end
-end
-
-function VehicleBar:UpdateButtonVisibility()
-	if IsVehicleAimAngleAdjustable() then
-		_G["VehicleMenuBarPitchUpButton"]:Show()
-		_G["VehicleMenuBarPitchDownButton"]:Show()
-	else
-		_G["VehicleMenuBarPitchUpButton"]:Hide()
-		_G["VehicleMenuBarPitchDownButton"]:Hide()
-	end
-
-	if CanExitVehicle() then
-		_G["VehicleMenuBarLeaveButton"]:Show()
-	else
-		_G["VehicleMenuBarLeaveButton"]:Hide()
-	end
 end
