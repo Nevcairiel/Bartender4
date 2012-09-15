b113a0feaeecfaeeb9f41926d5d7e5f9025166fe
Hendrik Leppkes <h.leppkes@gmail.com>
2012-09-15 08:16:55 +0200

    Use the esES locale for esMX, it appears the mexicans don't care to create their own.

--- locale/esES.lua
+++ locale/esES.lua
@@ -2,7 +2,7 @@
 -- Please use the Localization App on WoWAce to Update this
 -- http://www.wowace.com/projects/bartender4/localization/ ;¶
 
-local L = LibStub("AceLocale-3.0"):NewLocale("Bartender4", "esES")
+local L = LibStub("AceLocale-3.0"):NewLocale("Bartender4", "esES") or LibStub("AceLocale-3.0"):NewLocale("Bartender4", "esMX")
 if not L then return end
 
 --@localization(locale="esES", format="lua_additive_table", handle-unlocalized="comment")@
--- locale/esMX.lua
+++
@@ -1,8 +0,0 @@
--- Bartender4 Locale
--- Please use the Localization App on WoWAce to Update this
--- http://www.wowace.com/projects/bartender4/localization/ ;¶
-
-local L = LibStub("AceLocale-3.0"):NewLocale("Bartender4", "esMX")
-if not L then return end
-
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment")@
