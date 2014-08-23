088dcc1760e2ce8f96643777d20ce556ae46d6cd
Hendrik Leppkes <h.leppkes@gmail.com>
2014-08-23 16:48:11 +0200

    Fix warrior stance handling. Berserker Stance is gone, Gladiator Stance is new.

--- StateBar.lua
+++ StateBar.lua
@@ -82,9 +82,9 @@ local DefaultStanceMap = setmetatable({}, { __index = function(t,k)
 	local newT = nil
 	if k == "WARRIOR" then
 		newT = {
-			{ id = "battle", name = GetSpellInfo(2457), index = 1, type = "form"},
-			{ id = "def", name = GetSpellInfo(71), index = 2, type = "form"},
-			{ id = "berserker", name = GetSpellInfo(2458), index = 3, type = "form"},
+			{ id = "battle", name = GetSpellInfo(2457), index = 1 },
+			{ id = "def", name = GetSpellInfo(71), index = 2 },
+			{ id = "gladiator", name = GetSpellInfo(156291), index = 3 },
 		}
 	elseif k == "DRUID" then
 		newT = {
